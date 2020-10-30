e84e373d8cf65e846720a8cff8a861ab1124cb93
Jonathan Perry <jpper@umich.edu>
2020-10-30 14:16:33 +0900

    Finished part 5 (particle effects!)

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,6 +8,12 @@ public class LevelManager : MonoBehaviour
 
     private PlayerController player;
 
+    // Particle Game Objects
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+
+    public float respawnDelay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +23,28 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public void RespawnPlayer() {
+    public void RespawnPlayer()
+    {
+        StartCoroutine("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        // Launch the blood death particle system anim
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+        // Disable the player
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        // Put a pause between death and respawn
+        yield return new WaitForSeconds(respawnDelay);
+        // Re-enable both the player and renderer
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
         // Respawn player to first checkpoint
         player.transform.position = currentCheckpoint.transform.position;
+        Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+
     }
 }
