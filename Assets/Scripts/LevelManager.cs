703ae58740c36e1280c4fd42137ef82d87a3636d
Jonathan Perry <jpper@umich.edu>
2020-10-30 11:28:31 +0900

    Finish part 3 (checkpoints and respawning)

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject currentCheckpoint;
+
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RespawnPlayer() {
+        // Respawn player to first checkpoint
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+}
