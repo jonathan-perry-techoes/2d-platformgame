04f61a8fa8be21c0f7709e9bbb0cdc05e2fb1831
Jonathan Perry <jpper@umich.edu>
2020-11-11 00:16:56 +0900

    Finished episode 30 (level select map - moving between levels)

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelectManager : MonoBehaviour
 {
@@ -10,6 +8,15 @@ public class LevelSelectManager : MonoBehaviour
     public GameObject[] locks;
     public bool[] levelUnlocked;
 
+    public int positionSelector;
+    public float distanceBelowLock;
+
+    public string[] levelName;
+
+    public float moveSpeed;
+
+    private bool isPressed;
+    
     private void Start()
     {
         for (int i = 0; i < levelTags.Length; i++)
@@ -33,11 +40,58 @@ public class LevelSelectManager : MonoBehaviour
                 locks[i].SetActive(false);
             }
         }
+
+        transform.position = locks[positionSelector].transform.position + new Vector3(0, distanceBelowLock, 0);
     }
 
     private void Update()
     {
+        // If not pressed
+        if (!isPressed)
+        {
+            if (Input.GetAxis("Horizontal") > 0.25f)
+            {
+                positionSelector += 1;
+                isPressed = true;
+            }
+
+            if (Input.GetAxis("Horizontal") < -0.25f)
+            {
+                positionSelector -= 1;
+                isPressed = true;
+            }
+
+            // If going over the max, reset to end
+            if (positionSelector >= levelTags.Length)
+            {
+                positionSelector = levelTags.Length - 1;
+            }
+            // Error check for is selector is less than 0
+            if (positionSelector < 0)
+            {
+                positionSelector = 0;
+            }
+        }
         
-        
+        // If pressed
+        if (isPressed)
+        {
+            if (Input.GetAxis("Horizontal") < 0.25f && Input.GetAxis("Horizontal") > -0.25f)
+            {
+                isPressed = false;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position,
+        locks[positionSelector].transform.position + new Vector3(0, distanceBelowLock, 0),
+        moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
+        {
+            if (levelUnlocked[positionSelector])
+            {
+                SceneManager.LoadScene(levelName[positionSelector]);
+            }
+        }
     }
 }
